ebe787dead0815d06fd9b46140cb510dcd897352
anair123 <aashishnair8925@gmail.com>
2023-05-18 11:54:29 -0400

    update app.py

--- app.py
+++ app.py
@@ -30,22 +30,22 @@ def main():
 
     # get feature values
     amount_tsh = st.sidebar.slider(" Total static head (amount water available to waterpoint)", 0, 400000, 1)
-    funder = st.sidebar.selectbox('Who funded the well', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    gps_height = st.sidebar.slider("Altitude of the well", 0, 3000, 1)s
-    installer = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    basin = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    region = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    population = st.sidebar.slider("Artist's Number of Followers", 0, 40000, 1)
-    public_meeting = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    scheme_management = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    permit = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    extraction_type_class = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    management_group = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    payment_type = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    quality_group = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    quantity_group = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    source_class = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
-    waterpoint_type_group = st.sidebar.selectbox('Genre', ('Pop', 'Rock', 'Rap', 'Country', 'EDM', 'Other'))
+    funder = st.sidebar.selectbox('Who funded the well', ('Government Of Tanzania', 'Danida', 'Hesawa', 'Rwssp', 'World Bank', 'Kkkt','World Vision','Unicef', 'Other'))
+    gps_height = st.sidebar.slider("Altitude of the well", 0, 3000, 1)
+    installer = st.sidebar.selectbox('Installer', ('Government', 'DWE', 'RWE', 'Commu', 'DANIDA', 'Other'))
+    basin = st.sidebar.selectbox('Basin', ('Lake Victoria', 'Pangani', 'Rufiji', 'Internal', 'Lake Tanganyika', 'Wami / Ruvu', 'Lake Nyasa','Ruvuma / Southern Coast', 'Lake Rukwa'))
+    region = st.sidebar.selectbox('Region', ('Iringa', 'Shinyanga', 'Mbeya', 'Kilimanjaro', 'Morogoro', 'Arusha', 'Kagera', 'Mwanza', 'Kigoma', 'Ruvuma','Pwani', 'Tanga', 'Dodoma', 'Singida', 'Mara', 'Tabora', 'Rukwa', 'Mtwara', 'Manyara', 'Lindi', 'Dar es Salaam'))
+    population = st.sidebar.slider("Population of Community", 0, 40000, 1)
+    public_meeting = st.sidebar.selectbox('Public Meeting', ('TRUE', 'FALSE'))
+    scheme_management = st.sidebar.selectbox('Scheme Management', ('VWC', 'WUG', 'Water authority', 'WUA', 'Water Board', 'Parastatal', 'Private operator', 'Company', 'SWC', 'Trust', 'Other'))
+    permit = st.sidebar.selectbox('Permit', ('TRUE', 'FALSE'))
+    extraction_type_class = st.sidebar.selectbox('Extraction Type', ('gravity', 'handpump', 'submersible', 'motorpump', 'rope pump', 'wind-powered', 'other'))
+    management_group = st.sidebar.selectbox('Management Group', ('user-group', 'commercial', 'parastatal', 'unknown', 'other'))
+    payment_type = st.sidebar.selectbox('Payment Type', ('never pay', 'per bucket', 'monthly', 'on failure', 'annually', 'unknown','other'))
+    quality_group = st.sidebar.selectbox('Quality Group', ('good', 'salty', 'milky', 'colored', 'fluoride', 'unknown'))
+    quantity_group = st.sidebar.selectbox('Quantity Group', ('enough', 'insufficient', 'dry', 'seasonal', 'unknown'))
+    source_class = st.sidebar.selectbox('Source Class', ('groundwater', 'surface', 'unknown'))
+    waterpoint_type_group = st.sidebar.selectbox('Waterpoint Type Group', ('communal standpipe', 'hand pump', 'spring', 'cattle trough', 'dam', 'other'))
     age = st.sidebar.slider("Age of the Water Pump", 0, 100, 1)    
 
     # create input matrix with user response
@@ -66,12 +66,14 @@ def main():
     if st.button('Predict Water Pump Condition'):
         input_features_processed = pipeline_xgb.transform(input_features)
         prediction = xgb_model.predict(input_features_processed)[0]
-        st.success(f'Water Pump Condition: {prediction}')
+
+        prediction_map = {0:'Functional',
+                          1: 'Functional, but needs repair',
+                          2: 'Not Functional'}
+
+        st.success(f'Water Pump Condition: {prediction_map[prediction]}')
 
     pass
 
-['amount_tsh', 'funder', 'gps_height', 'installer', 'basin', 'region',
-       'population', 'public_meeting', 'scheme_management', 'permit',
-       'extraction_type_class', 'management_group', 'payment_type',
-       'quality_group', 'quantity_group', 'source_class',
-       'waterpoint_type_group', 'status_group', 'age']
+if __name__ == '__main__':
+    main()
